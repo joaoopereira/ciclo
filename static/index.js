13cf01f5c09f8762b694ca9dcbd24e52aa93bf14
João Pereira <joaopereira@criticalmanufacturing.com>
2021-04-23 20:09:59 +0100

    fix overlay issues on mobile

--- static/index.js
+++ static/index.js
@@ -50,8 +50,7 @@ function bindEvents() {
     });
 }
 
-function trmarkup(colCount)
-{
+function trmarkup(colCount) {
     var trmarkup = '<tr class="table-dark">';
     for (i = 0; i < colCount; i++) {
         trmarkup += '<td><video width="100%" loop muted playsinline><source></video></td>';
@@ -94,28 +93,28 @@ function setVideoSources() {
                     var hls = new Hls();
                     hls.loadSource(videoSrc);
                     hls.attachMedia(video);
-                  }
-                  // hls.js is not supported on platforms that do not have Media Source
-                  // Extensions (MSE) enabled.
-                  //
-                  // When the browser has built-in HLS support (check using `canPlayType`),
-                  // we can provide an HLS manifest (i.e. .m3u8 URL) directly to the video
-                  // element through the `src` property. This is using the built-in support
-                  // of the plain video element, without using hls.js.
-                  //
-                  // Note: it would be more normal to wait on the 'canplay' event below however
-                  // on Safari (where you are most likely to find built-in HLS support) the
-                  // video.src URL must be on the user-driven white-list before a 'canplay'
-                  // event will be emitted; the last video event that can be reliably
-                  // listened-for when the URL is not on the white-list is 'loadedmetadata'.
-                  else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+                }
+                // hls.js is not supported on platforms that do not have Media Source
+                // Extensions (MSE) enabled.
+                //
+                // When the browser has built-in HLS support (check using `canPlayType`),
+                // we can provide an HLS manifest (i.e. .m3u8 URL) directly to the video
+                // element through the `src` property. This is using the built-in support
+                // of the plain video element, without using hls.js.
+                //
+                // Note: it would be more normal to wait on the 'canplay' event below however
+                // on Safari (where you are most likely to find built-in HLS support) the
+                // video.src URL must be on the user-driven white-list before a 'canplay'
+                // event will be emitted; the last video event that can be reliably
+                // listened-for when the URL is not on the white-list is 'loadedmetadata'.
+                else if (video.canPlayType('application/vnd.apple.mpegurl')) {
                     video.src = videoSrc;
-                  }
-                
+                }
+
                 i++;
             });
         },
-        error: function() {
+        error: function () {
             console.log('Error occured');
         }
     });
@@ -123,14 +122,14 @@ function setVideoSources() {
 
 
 $(document).ready(function () {
+
+    buildTable();
+
+    setVideoSources();
+
+    bindEvents();
     $('#overlay').click(function () {
         this.style.display = "none";
-        buildTable();
-
-        setVideoSources();
-    
-        bindEvents();
-        
         playAll();
     });
 });
